df03180d1ac76214db069bf9506ae29abf318f6c
Kuhan Loh <kuhan.loh1@ibm.com>
2018-11-05 22:10:55 +0000

    added deepmerge, as object.assign doesnt deep copy

--- src/store/reducers/fuelSavingsReducer.js
+++ src/store/reducers/fuelSavingsReducer.js
@@ -1,6 +1,6 @@
 import { SAVE_FUEL_SAVINGS, CALCULATE_FUEL_SAVINGS } from '../actions/actions'
 import { necessaryDataIsProvidedToCalculateSavings, calculateSavings } from '../../utils/fuelSavings'
-import objectAssign from 'object-assign'
+import merge from 'deepmerge'
 import initialState from './initialState'
 
 // IMPORTANT: Note that with Redux, state should NEVER be changed.
@@ -15,10 +15,10 @@ export default function fuelSavingsReducer (state = initialState.fuelSavings, ac
     case SAVE_FUEL_SAVINGS:
       // For this example, just simulating a save by changing date modified.
       // In a real app using Redux, you might use redux-thunk and handle the async call in fuelSavingsActions.js
-      return objectAssign({}, state, { dateModified: action.dateModified })
+      return merge(state, { dateModified: action.dateModified })
 
     case CALCULATE_FUEL_SAVINGS:
-      newState = objectAssign({}, state)
+      newState = Object.assign({}, state)
       newState[action.fieldName] = action.value
       newState.necessaryDataIsProvidedToCalculateSavings = necessaryDataIsProvidedToCalculateSavings(newState)
       newState.dateModified = action.dateModified
